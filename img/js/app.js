068f661b2b59a8b9ced1e2fef8eb84d3757d199d
Cauã Braga de Lima <cauabrgal11@gmail.com>
2023-02-01 08:27:01 -0300

    1st commit

---
+++ img/js/app.js
@@ -0,0 +1,22 @@
+//declara a função para o efeito toggle:
+const getElement = (selector) => 
+{
+  const element = document.querySelector(selector)
+
+  if (element) return element
+  throw Error(
+    `Please double check your class names, there is no ${selector} class`
+  )
+}
+
+//conjunto de links: recebe a função criada e passa a classe dos links como parâmetro:
+const links = getElement('.hero')
+
+//botão de toggle: recebe a função criada e passa a classe do botão como parâmetro:
+const navBtnDOM = getElement('.nav-btn')
+
+//adiciona o evento ao botão de toggle que mostra os links uma vez clicado:
+navBtnDOM.addEventListener('click', () => 
+{
+  links.classList.toggle('show-links')
+})
