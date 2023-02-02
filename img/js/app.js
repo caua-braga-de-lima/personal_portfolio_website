a298a32f322abd0dc3af68a388d2c1e10d95050e
Cauã Braga de Lima <cauabrgal11@gmail.com>
2023-02-02 16:07:29 -0300

    main-hero

---
+++ img/js/app.js
@@ -0,0 +1,27 @@
+//declara a função para o efeito toggle:
+const getElement = (selector) => 
+{
+  const element = document.querySelector(selector)
+
+  if (element) return element
+  throw Error(
+    `Please double check your class names, there is no ${selector} class`
+  )
+}
+
+//conjunto de links: recebe a função criada e passa a classe dos links como parâmetro:
+const links = getElement('.list-of-links')
+
+//botão de toggle: recebe a função criada e passa a classe do botão como parâmetro:
+const navBtnDOM = getElement('.toggle')
+
+//adiciona o evento ao botão de toggle que mostra os links uma vez clicado:
+navBtnDOM.addEventListener('click', () => 
+{
+  links.classList.toggle('.show-links')
+})
+
+ // cofigura a atualização dinâmica da data no rodapé da página:
+// const date = getElement('#date')
+// const currentYear = new Date().getFullYear()
+// date.textContent = currentYear
